40c135231a673702f774b489f9450f18eb88c627
ekoneko <9@ikaze.me>
2017-09-10 23:59:47 +0800

    server-worker: fake fetch request

--- service-worker/code/index.ts
+++ service-worker/code/index.ts
@@ -6,15 +6,26 @@ window.onload = () => {
             console.log(`ServiceWorker registration successful with scope: ${registration.scope}`)
         })
 
-    const text = document.getElementById('text')
+    const textArea = <HTMLTextAreaElement>document.getElementById('text')
     const saveBtn = document.getElementById('save')
     const loadBtn = document.getElementById('load')
 
     saveBtn.addEventListener('click', (ev) => {
         // TODO(kaze): post to `/api/data` save the text.value (put to cacheStorge)
+        fetch('/api/data', {
+            method: 'POST',
+            body: textArea.value,
+        })
     })
 
     loadBtn.addEventListener('click', (ev) => {
         // TODO(kaze): post to `/api/data` load the text.value (get from cacheStorge)
+        fetch('/api/data', {
+            method: 'GET'
+        })
+        .then(response => response.text())
+        .then(text => {
+            textArea.value = text
+        })
     })
 }
--- service-worker/code/sw.ts
+++ service-worker/code/sw.ts
@@ -21,17 +21,30 @@ self.addEventListener('install', (ev: InstallEvent) => {
 
 self.addEventListener('fetch', (ev: FetchEvent) => {
     const {request} = ev
-    ev.respondWith(
-        caches.open('my-test-cahce-v1')
-            .then(cache => cache.match(request))
-            .then(response => {
-                if (response) {
-                    console.log(`Cache [${request.method}] ${request.url}`)
-                    return response
-                } else {
-                    console.log(`Fetch [${request.method}] ${request.url}`)
-                    return fetch(request)
-                }
-            })
-    )
+    ev.respondWith(fetchRespondWith(request))
 })
+
+async function fetchRespondWith(request: Request) {
+    const cache = await caches.open('my-test-cahce-v1');
+    const cacheResponse = await cache.match(request)
+    if (cacheResponse) {
+        console.log(`Cache [${request.method}] ${request.url}`)
+        return cacheResponse
+    }
+
+    if (request.url.match(`${location.origin}/api/data`) && request.method === 'POST') {
+        const cacheRequest = new Request(request.url, {
+            method: 'GET',
+            headers: request.headers,
+            mode: request.mode,
+            credentials: request.credentials,
+            redirect: request.redirect
+        })
+        const body = await request.text()
+        cache.put(cacheRequest, new Response(body))
+        return new Response()
+    }
+    console.log(`Fetch [${request.method}] ${request.url}`)
+    const response = await fetch(request)
+    return response
+}
